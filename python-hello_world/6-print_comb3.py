42f8342ffcc494adf0f5de1397f934b5d2b3e9f8
Abukmayen <abukmayen123@gmail.com>
2023-07-17 05:38:25 -0700

    6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -2,4 +2,4 @@
 
 for tens_digit in range(10):
     for units_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, units_digit), end=", " if tens_digit < 8 else "\n")
+        print("{:02d}".format(tens_digit * 10 + units_digit), end=", " if tens_digit < 8 else "\n")
